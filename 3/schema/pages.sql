b8dbe0d59822d428148f1ffb960cc2d179d3052e
Volodymyr Melnyk <vmelnyk@tradetracker.com>
2018-10-18 13:57:06 +0300

    adds server with DB interaction: 3/serverWithDB.go

---
+++ 3/schema/pages.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE cms;
+USE cms;
+
+CREATE TABLE `pages` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `page_guid` varchar(256) NOT NULL DEFAULT '',
+  `page_title` varchar(256) DEFAULT NULL,
+  `page_content` mediumtext,
+  `page_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `page_guid` (`page_guid`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+INSERT INTO `pages` (`id`, `page_guid`, `page_title`, `page_content`, `page_date`) VALUES (NULL, 'hello-world', 'Hello, World!', 'Hey! How are you doin?', CURRENT_TIMESTAMP);
