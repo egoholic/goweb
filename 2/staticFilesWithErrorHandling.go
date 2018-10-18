3c132fd44278efd0ae5288bd0845e3d91e8d89c5
Volodymyr Melnyk <vmelnyk@tradetracker.com>
2018-10-18 12:20:08 +0300

    adds staticFilesWithErrorHandling.go

---
+++ 2/staticFilesWithErrorHandling.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+)
+
+const (
+	PORT = ":8080"
+)
+
+func pageHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	pageID := vars["id"]
+	fileName := "./var/www/" + pageID + ".html"
+	_, err := os.Stat(fileName)
+	if err != nil {
+		fileName = "./var/www/404.html"
+	}
+	http.ServeFile(w, r, fileName)
+}
+
+func main() {
+	rtr := mux.NewRouter()
+	rtr.HandleFunc("/pages/{id:[0-9]+}", pageHandler)
+	http.Handle("/", rtr)
+	http.ListenAndServe(PORT, nil)
+}
